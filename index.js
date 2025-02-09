282e01ec63051e2f25f3fca75331f6ecafc78a2e
nischalmeht <58307277+nischalmeht@users.noreply.github.com>
2025-02-09 21:56:24 +0530

    fix

--- index.js
+++ index.js
@@ -20,49 +20,22 @@ app.use(function(req,res,next){
   console.log("middleware")
   next()
 })
-// app.get('/', (req, res) => {
-//   fs.readdir('./files',function(err,files){ 
-//     res.render('index',{files:files})
-//   })
-// })
 app.get('/', (req, res) => {
-  fs.readdir('./files', (err, files) => {
-      if (err) {
-          return res.status(500).json({ message: 'Error reading directory', error: err });
-      }
+  fs.readdir('./files',function(err,files){
+    console.log(files,'files')
+    res.render('index',{files:files})
+  })
+})
+app.get('/file/:filename', (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`,'utf-8',function(err,files){
+    console.log(files)
+    res.render("show",{filename:req.params.filename,fileData:files})
+  })
+  
+})
 
-      let fileData = [];
-
-      // Read each file and parse its content
-      let filesRead = 0;
-      files.forEach((file, index) => {
-          fs.readFile(`./files/${file}`, 'utf8', (err, data) => {
-              filesRead++;
-              if (!err) {
-                  try {
-                      fileData.push(JSON.parse(data)); // Parse JSON data
-                  } catch (parseErr) {
-                      console.error(`Error parsing file ${file}:`, parseErr);
-                  }
-              }
-
-              // Send response after reading all files
-              if (filesRead === files.length) {
-                  res.render('index', { files: fileData }); // Pass file data to template
-              }
-              console.log(fileData,'fileData')
-          });
-      });
-
-      // Handle case where there are no files
-      if (files.length === 0) {
-          res.render('index', { files: [] });
-      }
-  });
-});
 app.post('/create', (req, res) => {
-  const jsonData = JSON.stringify(req.body, null, 2);
- fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,jsonData,function(err){
+ fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
    res.redirect('/')
  })
 })
